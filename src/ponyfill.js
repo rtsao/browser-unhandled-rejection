5a32ae936f54c327ded34e39dfed023dc9eec291
Ryan Tsao <ryan.j.tsao@gmail.com>
2017-04-13 09:02:17 -0700

    [ci skip] Update comment

--- src/ponyfill.js
+++ src/ponyfill.js
@@ -1,9 +1,10 @@
 const OriginalPromise = window.Promise;
 
 /**
- * ES5 subclassing is used because Promise might not be is not subclassable:
+ * ES5 subclassing is used per:
+ * https://github.com/rtsao/browser-unhandled-rejection/issues/1
  * https://kangax.github.io/compat-table/es6/#test-Promise_is_subclassable
- * 
+ *
  * Adapted from: https://gist.github.com/domenic/8ed6048b187ee8f2ec75
  */
 const InstrumentedPromise = function Promise(resolver) {
