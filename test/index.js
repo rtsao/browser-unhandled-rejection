92b6f0d4277fef9732756fb623942fd5af187eb1
Ryan Tsao <ryan.j.tsao@gmail.com>
2017-04-19 14:22:20 -0700

    Update test

--- test/index.js
+++ test/index.js
@@ -2,10 +2,21 @@ import {auto} from '../src/index.js';
 import test from 'tape';
 
 test('auto polyfill works', t => {
-  t.plan(1);
-  window.addEventListener('unhandledrejection', () => {
+  t.plan(4);
+  const reason = 'rejection';
+  let promise;
+  const listener = (e) => {
     t.pass('unhandledrejection event fired');
-  });
+    /**
+     * Test attributes according to:
+     * https://html.spec.whatwg.org/multipage/webappapis.html#unhandled-promise-rejections
+     */
+    t.equal(e.reason, reason, 'reason attribute matches expected value');
+    t.equal(e.promise, promise, 'promise attribute matches expected value');
+    t.equal(e.cancelable, true, 'cancelable attribute is true');
+    window.removeEventListener('unhandledrejection', listener);
+  }
+  window.addEventListener('unhandledrejection', listener);
   auto();
-  Promise.reject('rejection');
+  promise = Promise.reject(reason);
 });
